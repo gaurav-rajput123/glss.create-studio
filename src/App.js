f6a0b0a6044d9d5f32bc1fe8d60a3c5bd3f7b5ea
Gaurav <rajputgr513gssm@gmail.com>
2022-02-01 16:28:53 +0530

    asdf

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import courseArray from './resources/courseArray'
-import Middle from './namrata/Middle'
-import SubjectTile from './namrata/SubjectTile'
+import SubjectTitle from './namrata/SubjectTile'
+import { TextField } from '@mui/material';
+import { useState } from 'react';
 function App() {
+  const [val, setVal] = useState("hello")
   return (
     <div className="App">
-      
-        <SubjectTile/>
-      
+      <SubjectTitle/>
+      <TextField value={val} onChange={(e)=>setVal(e.target.value)} label="hello"/>
     </div>
   );
+
+
+
+  // {courseArray.map((item, index) => {
+  //   return (
+  //     <div className='mainCourse'>
+  //       <h4>{item.name}</h4>
+  //       <div className='course-content'>
+  //         {
+  //           item.topics.map((topic, index)=>{
+  //             return(<div className="topic-tile">
+  //               <h5>{topic.name}</h5>
+  //               <div className='topic-contents'>
+  //                 {topic.subTopics.map((subtopic, index)=>{
+  //                   return (<div className='subtopicTile'>
+  //                     <h6>{subtopic.name}</h6>
+  //                     {/* <div>
+  //                       {subtopic.}
+  //                     </div> */}c
+  //                   </div>)
+  //                 })}
+  //               </div>
+  //             </div>)
+  //           })
+  //         }
+  //       </div>
+  //     </div>
+  //   )
+  // })}
 }
-
-
-
-
-// {courseArray.map((item, index) => {
-//   return (
-//     <div className='mainCourse'>
-//       <h4>{item.name}</h4>
-//       <div className='course-content'>
-//         {
-//           item.topics.map((topic, index)=>{
-//             return(<div className="topic-tile">
-//               <h5>{topic.name}</h5>
-//               <div className='topic-contents'>
-//                 {topic.subTopics.map((subtopic, index)=>{
-//                   return (<div className='subtopicTile'>
-//                     <h6>{subtopic.name}</h6>
-//                     {/* <div>
-//                       {subtopic.}
-//                     </div> */}
-//                   </div>)
-//                 })}
-//               </div>
-//             </div>)
-//           })
-//         }
-//       </div>
-//     </div>
-//   )
-// })}
 
 export default App;
---
+++ src/namrata/TextNLabel.js
@@ -0,0 +1,38 @@
+import { TextField } from '@mui/material';
+import { Button } from '@mui/material';
+import { Typography } from '@mui/material';
+import React, { useState } from 'react';
+
+function TextNLabel() {
+    const [title, setTitle] = useState('Section')
+    const [isLabel, setIsLabel] = useState(false)
+    const [label, setLabel] = useState("Title")
+
+    return (
+        isLabel ?
+            (
+                <div onClick={()=>setIsLabel(!isLabel)}>
+                    <Typography >
+                    {label}
+                </Typography>
+                </div>
+            )
+            :
+            (
+                <div>
+                    <TextField
+                        value={label}
+                        onChange={(e) => setLabel(e.target.value)}
+                        label="hello brother from another mother" />
+                    <Button onClick={() => setIsLabel(!isLabel)}>
+                        <Typography>
+                            Set Title
+                        </Typography>
+                    
+                    </Button>
+                </div>
+            )
+    )
+}
+
+export default TextNLabel;
